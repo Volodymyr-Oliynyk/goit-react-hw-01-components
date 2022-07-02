76f46df8a82738e1d6adcddb484b1016ecab53f7
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-07-03 00:54:34 +0300

    start_statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import Profile from "./Profile/UserProfile";
 import user from "../data/user.json";
-
+import Statistics from "./Statistics/Statistics";
+import data from "../data/data.json";
 export const App = () => {
-  return (
-  <Profile
+  return ( <div>
+    <Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
   />
-);
+ <Statistics title="Upload stats" stats={data} />
+<Statistics stats={data} />
+  </div>
+  );
 };
